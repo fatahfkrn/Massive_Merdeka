d3140cce21937e5816a9cee98577eb2a2a7e54ff
fatahfkrn <fatahfkrn@gmail.com>
2024-05-26 05:31:31 +0700

    update beberapa fitur

--- src/ui/Beranda.jsx
+++ src/ui/Beranda.jsx
@@ -1,12 +1,18 @@
 import Header from "./Header";
 import Carousel from "./Carousel";
 import Footer from "./Footer";
+import AboutDispora from "./AboutDispora";
+import OpenTrip from "./OpenTrip";
+import Feedback from "./feedback";
 
 const Beranda = () => {
     return (
         <div>
             <Header />
             <Carousel />
+            <AboutDispora />
+            <OpenTrip />
+            <Feedback />
             <Footer />
         </div>
     );
